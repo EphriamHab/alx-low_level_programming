889414e2fb568cc0ce800040674300437ac253a2
EphriamHab <ephremhabtamu2015@gmail.com>
2022-12-08 07:50:26 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the phrase with proper grammar ...
+ * Return: 0 if exited properly, non-zero otherwise
+   */
+
+
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+}
