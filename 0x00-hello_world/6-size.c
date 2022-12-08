ebc54cd2318c8b673e669032e984418714538e97
EphriamHab <ephremhabtamu2015@gmail.com>
2022-12-08 10:44:33 -0800

    size of()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 
 
 /** 
@@ -7,8 +7,8 @@
  */
 
 int main(void)
-
 {
+
 
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
