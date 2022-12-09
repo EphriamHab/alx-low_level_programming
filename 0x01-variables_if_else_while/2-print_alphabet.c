693948730c68116bdd313395fce037861441ba6c
EphriamHab <ephremhabtamu2015@gmail.com>
2022-12-09 06:07:26 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main Entery
+ * Return always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for(c = 'a';c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}	
