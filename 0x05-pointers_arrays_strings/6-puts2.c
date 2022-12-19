84a01200fa3eb189697dcfaa396d8bc9d1e246c1
EphriamHab <ephremhabtamu2015@gmail.com>
2022-12-19 10:42:53 +0300

    pointers,array and string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,16 @@
  */
 void puts2(char *str)
 {
-	int i, x;
-i = 0;
-x = 0;
-	while (str[i++])
+	int j, a;
+j = 0;
+a = 0;
+	while (str[j++])
 	{
-		x++;
+		a++;
 	}
-for (i = 0; i < x; i += 2)
+for (j = 0; j < x; j += 2)
 {
-_putchar(*(str + i));
+_putchar(*(str + j));
 }
 _putchar('\n');
 }
