c280c3933d3ab5968f0374c30f8f4f66639fa285
EphriamHab <ephremhabtamu2015@gmail.com>
2023-01-06 03:20:31 -0800

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the program name, followed by a new line.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argv: An array of pointers to the arguments
  *
  * Return: Always o.
  */
