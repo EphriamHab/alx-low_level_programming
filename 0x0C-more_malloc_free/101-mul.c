bcd96e2c2a9730f62c2a71f3495d389666632c62
EphriamHab <ephremhabtamu2015@gmail.com>
2023-01-12 01:49:01 -0800

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * _is_zero - determines if any number is zero
+ * _is_zero - determines if any number is zero.
  * @argv: argument vector.
  *
  * Return: no return.
