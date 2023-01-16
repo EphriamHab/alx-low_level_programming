649e16d75abe115eb8004f874decc5252bcfd60f
EphriamHab <ephremhabtamu2015@gmail.com>
2023-01-16 14:11:02 -0800

    Outside of a dog, a book is a man's best friend.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 /**
  *struct dog - a new type
- *@name : string
+ *@name : string.
  *@age : float
  *@owner : string
  *Description: a new type that contains dog name, age, and owner.
