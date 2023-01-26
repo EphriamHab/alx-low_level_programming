54f8a7a3708a0419d2f043e6cb107a9de86b561e
EphriamHab <ephremhabtamu2015@gmail.com>
2023-01-26 02:50:25 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * execute_before_main - executes this function before main function
+ * execute_before_main - executes this function before main function.
  * Note: made possible by prior declaration of "__attribute__ ((constructor))"
  */
 
