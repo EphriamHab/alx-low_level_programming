7e9550b83fb77e1d0d583656c1256bc3c8b6b213
EphriamHab <ephremhabtamu2015@gmail.com>
2023-01-31 14:46:01 +0300

    project on single linked list commmited

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
-  * print_listint - prints all the elements of a listint_t list
-  * @h: pointer to head of const listint_t
-  * Return: the number of nodes in h
-  */
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to head of const listint_t
+ * Return: the number of nodes in h
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0; /* number of nodes printed */
+	size_t count = 0;
 
 	while (h)
 	{
